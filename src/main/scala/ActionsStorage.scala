7cd09f9f326fe4e0c5281ea7424f5d391edf9a62
Vadim <vadopolski@gmail.com>
2021-03-18 01:11:53 +0300

    Update

--- src/main/scala/ActionsStorage.scala
+++ src/main/scala/ActionsStorage.scala
@@ -3,67 +3,75 @@ import akka.actor.typed.scaladsl.Behaviors
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.model.{ContentTypes, HttpEntity, StatusCodes}
 import akka.http.scaladsl.server.Directives._
+import akka.http.scaladsl.server.Route
 import spray.json.{DefaultJsonProtocol, enrichAny, _}
 
 import scala.concurrent.duration._
 import scala.io.StdIn
 import scala.util.{Failure, Success}
 
-case class Action(id: Int, name: String)
+case class Action(id: Int, value: String)
 
-trait ActionJsonProtocol extends DefaultJsonProtocol {
-  implicit val actionJson = jsonFormat2(Action)
+trait JsonProtocol extends DefaultJsonProtocol {
+  implicit val action = jsonFormat2(Action)
 }
 
-object ActionsStorage extends ActionJsonProtocol{
-  var actions = List(
-    Action(1, "Add"),
-    Action(2, "Multiply"),
-    Action(3, "Divide")
+object ActionsStorage extends JsonProtocol{
+  var dbMock = List(
+    Action(1, "add"),
+    Action(2, "divide")
   )
 
-  def main(args: Array[String]): Unit = {
-    implicit val system           = ActorSystem(Behaviors.empty, "my-system")
-    implicit val executionContext = system.executionContext
+  def executeAction(input: String): Unit = {
+    input.parseJson.convertTo[Action]
+  }
 
-    val personServerRoute =
-      pathPrefix("api" / "action") {
-        get {
-          (path(IntNumber) | parameter('id.as[Int])) { id =>
-            complete(
-              HttpEntity(
-                ContentTypes.`application/json`,
-                actions.find(_.id == id).toJson.prettyPrint
-              )
+
+  implicit val system           = ActorSystem(Behaviors.empty, "my-system")
+  implicit val executionContext = system.executionContext
+
+
+
+  val route: Route = pathPrefix("api" / "action") {
+    get {
+      pathEndOrSingleSlash {
+        complete(
+          HttpEntity(
+            ContentTypes.`application/json`,
+            dbMock.toJson.prettyPrint
+          )
+        )
+      } ~
+        (path(IntNumber) | parameter('id.as[Int])) { id =>
+          complete(
+            HttpEntity(
+              ContentTypes.`application/json`,
+              dbMock.find(_.id == id).toJson.prettyPrint
             )
-          } ~
-          pathEndOrSingleSlash {
-            complete(
-              HttpEntity(
-                ContentTypes.`application/json`,
-                actions.toJson.prettyPrint
-              )
-            )
-          }
-        } ~
-          (post & pathEndOrSingleSlash & extractRequest & extractLog) { (request, log) =>
-            val entity             = request.entity
-            val strictEntityFuture = entity.toStrict(2 seconds)
-            val actionFuture       = strictEntityFuture.map(_.data.utf8String.parseJson.convertTo[Action])
+          )
+        }
+    } ~
+      (post & pathEndOrSingleSlash & extractRequest & extractLog) { (request, log) =>
+        val entity             = request.entity
+        val strictEntityFuture = entity.toStrict(2 seconds)
+        val actionFuture       = strictEntityFuture.map(_.data.utf8String.parseJson.convertTo[Action])
 
-            onComplete(actionFuture) {
-              case Success(action) =>
-                log.info(s"Got action: $action")
+        onComplete(actionFuture) {
+          case Success(action) =>
+            log.info(s"Got action: $action")
 
-                actions = actions :+ action
-                complete(StatusCodes.OK)
-              case Failure(ex) =>
-                failWith(ex)
-            }
-          }
+            dbMock = dbMock :+ action   // db write
+
+            complete(StatusCodes.OK)
+          case Failure(ex) =>
+            failWith(ex)
+        }
       }
+  }
 
-    val bindingFuture = Http().newServerAt("localhost", 8000).bind(personServerRoute)
+
+  def main(args: Array[String]): Unit = {
+    val bindingFuture = Http().newServerAt("localhost", 8000).bind(route)
 
     println(s"Server online at http://localhost:8000/\nPress RETURN to stop...")
     StdIn.readLine() // let it run until user presses return
